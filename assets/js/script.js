ae2d7a3c6ef614cf6a0728743fef0eef6659212f
zarah Christenson <zarahc83@gmail.com>
2023-12-10 21:00:48 +0000

    style html and javascript

---
+++ assets/js/script.js
@@ -0,0 +1,134 @@
+const questions = [
+    {
+        question: "Which disney princess sings 'Once upon a time'?",
+        option1: "Cinderella",
+        option2: "Aorora",
+        option3: "snow-white",
+        option4: "Ariel",
+        answer: 2
+    },
+{
+    question: "What are Cinderella's stepsisters called?",
+    option1: "Cornelia and Amanda",
+    option2: "Betty and annie",
+    option3: "Anastasia och Drizella",
+    option4: "Alicia and Minnie",
+    answer: 3
+},
+
+{
+    question: "How many brothers does Prince Hans of the Southern Isles have in Frozen?",
+    option1: "12",
+    option2: "7",
+    option3: "14",
+    option4: "10",
+    answer: 1
+},
+
+{
+    question: "What does 'Hakuna Matata' mean?",
+    option1: "seize the day",
+    option2: "No worries",
+    option3: "Leave it",
+    option4: "Don't care",
+    answer: 2
+},
+
+{
+    question: "Who is Mufasa's trusted advisor in The Lion King?",
+    option1: "scar",
+    option2: "zazu",
+    option3: "Zarabi",
+    option4: "Rafiki",
+    answer: 2
+},
+
+{
+    question: "Who is Goofy's son?",
+    option1: "John",
+    option2: "Sebastian",
+    option3: "Peter",
+    option4: "Maximilian",
+    answer: 4
+},
+
+{
+    question: "What is the name of Sleeping Beauty's prince?",
+    option1: "Prins Philip",
+    option2: "Prins Eric",
+    option3: "Prins John",
+    option4: "Prins Alladin",
+    answer: 1
+},
+
+{
+    question: "Pongo and Perdita originally had how many puppies in 101 Dalmatians?",
+    option1: "8",
+    option2: "17",
+    option3: "15",
+    option4: "11",
+    answer: 3
+},
+
+{
+    question: "Pongo and Perdita originally had how many puppies in 101 Dalmatians?",
+    option1: "8",
+    option2: "17",
+    option3: "15",
+    option4: "11",
+    answer: 3
+},
+
+{
+    question: "What were Mickey Mouse's first ever spoken words?",
+    option1: "Banana",
+    option2: "Hot dog",
+    option3: "Hello",
+    option4: "pip",
+    answer: 2
+},
+
+{
+    question: "Who is Miguel's idol in Coco?",
+    option1: "Rocío Jurado",
+    option2: "Rafael Nadal",
+    option3: "Salvador Dalí",
+    option4: "Ernesto de la Cruz",
+    answer: 2
+},
+
+{
+    question : "What is the name of the Italian restaurant where Lady and the Tramp share spaghetti?",
+    option1: "Tony's restaurant",
+    option2: "Danni's restaurant",
+    option3: "Mike's restaurant",
+    option4: "Ellas's restaurant",
+    answer: 1
+},
+
+{
+    question : "What was the name of Simba's evil uncle?",
+    option1: "Scar",
+    option2: "Blood",
+    option3: "brain",
+    option4: "diseas",
+    answer: 1
+},
+
+{
+    question : "Quasimodo was the bell ringer of which famous cathedral?",
+    option1: "maria del mar",
+    option2: "Sevilla",
+    option3: "Notre Dame",
+    option4: "Primada",
+    answer: 3
+},
+{
+    question : "What is the name of Pocahontas' raccoon?",
+    option1: "Pichu",
+    option2: "Neko",
+    option3: "Stitch",
+    option4: "Figarro",
+    answer: 3
+},
+]
